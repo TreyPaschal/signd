6500b94ee643123a3729a25ed1297852f679edf4
Paschal <trey.paschal@c02vpb5jhtd6.echostar.com>
2022-04-29 11:16:10 -0600

    Update heading

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <nav className="border-b p-6">
-        <p className="text-4xl font-bold">Metaverse Marketplace</p>
+        <p className="text-4xl font-bold">Welcome to Signd</p>
+        <p className="text-2xl font-bold">Buy Sell and Create NFT Assets</p>
         <div className="flex mt-4">
           <Link href="/">
             <a className="mr-4 text-pink-500">
